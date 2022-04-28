dcbeee479ecd743d02f7986f5cbd4156360101a7
Akanksha <an9sx@virginia.edu>
2022-04-27 22:37:17 -0500

    Added redis client template for cache interface

---
+++ internal/redis/redis.go
@@ -0,0 +1,60 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// Config contains Redis client configuration
+type Config struct {
+	Host     string
+	Port     int
+	Password string
+}
+
+// Client is a wrapper for Redis Client
+type Client struct {
+	Client *redis.Client
+	Config *Config
+	logger *log.Logger
+}
+
+// NewRedisConfig return Redis client config
+func NewRedisConfig() *Config {
+	cfg := Config{}
+	return &cfg
+}
+
+// New creates a new redis client - TODO. This is just for wire.go
+func New(log *log.Logger, config *Config) *Client {
+	addr := fmt.Sprintf("%s:%d", config.Host, config.Port)
+	rd := Client{
+		logger: log,
+		Config: config,
+		Client: redis.NewClient(&redis.Options{
+			Addr:     addr,
+			Password: config.Password,
+		}),
+	}
+
+	return &rd
+}
+
+// Get TODO
+func (rd *Client) Get(ctx context.Context, key string) (interface{}, bool, error) {
+	return nil, false, nil
+}
+
+// Set TODO
+func (rd *Client) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+	return nil
+}
+
+// Delete TODO
+func (rd *Client) Delete(ctx context.Context, keys ...string) error {
+	return nil
+}
